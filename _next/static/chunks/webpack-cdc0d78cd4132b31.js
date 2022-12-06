e97f4252c0aad129f72166f3dc2ce63283b505a4
bangpateng <coinnyari@gmail.com>
2022-12-07 02:49:17 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-cdc0d78cd4132b31.js
+++ _next/static/chunks/webpack-cdc0d78cd4132b31.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "b17bd6bfbfa632e6" + ".css";
+/******/ 			return "static/css/" + "1edefc8420efd8a8" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
